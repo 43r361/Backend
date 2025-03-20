a47bccb8caf2c1246c88cae06c62ba7cb8a92f97
Alexander Shestakov <alexandorshestakov@gmail.com>
2025-03-20 15:17:08 +0200

    configured server

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,40 @@
-import { NestFactory } from "@nestjs/core";
+import { ClassSerializerInterceptor, ValidationPipe } from "@nestjs/common";
+import { NestFactory, Reflector } from "@nestjs/core";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { SwaggerTheme, SwaggerThemeNameEnum } from "swagger-themes";
 
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 
+	app.useGlobalPipes(
+		new ValidationPipe({
+			whitelist: true,
+			forbidNonWhitelisted: true,
+		})
+	);
+
+	app.useGlobalInterceptors(
+		new ClassSerializerInterceptor(app.get(Reflector))
+	);
+
+	const config = new DocumentBuilder()
+		.setTitle("43r361 API")
+		.setVersion("1.0.0")
+		.addServer("http://localhost:8393")
+		.build();
+
+	const document = SwaggerModule.createDocument(app, config);
+
+	const theme = new SwaggerTheme();
+
+	SwaggerModule.setup("api", app, document, {
+		customCss: theme.getBuffer(SwaggerThemeNameEnum.DARK),
+		customSiteTitle: "43r361 API Docs",
+	});
+
 	await app.listen(process.env.PORT ?? 8393);
 }
+
 bootstrap();
